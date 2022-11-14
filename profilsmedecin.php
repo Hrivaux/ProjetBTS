f4267aaa52d9b470eed79504c915b694c2aac9cc
Valentin <valentin.ctx@gmail.com>
2022-11-14 16:31:09 +0100

    update

---
+++ profilsmedecin.php
@@ -0,0 +1,22 @@
+<HTML>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>4</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="py-20">
+
+<h1 class="text-2xl ">test </h1>
+
+<img class="bg-color-red" src="img/cyril.jpg">
+
+
+<?php
+
+?>
+
+</body>
+</html>
