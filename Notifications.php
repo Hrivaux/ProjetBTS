96f13d49e2a5ef7e4e2b173836401593ca2c1208
Valentin <valentin.ctx@gmail.com>
2022-11-15 17:15:23 +0100

    e

--- Notifications.php
+++ Notifications.php
@@ -75,8 +75,8 @@ connected_only();
                                                             <label>Message</label>
                                                             <input type="text" class="form-control" name="message" rows="3" placeholder="Quelle notification voulez-vous envoyer?">
                                                        </div>
-                                                <label for="pet-select" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">le Niveau d'urgence :</label>
-                                                        <select id="urgence" name="urgence" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                                <label for="pet-select" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Le niveau d'urgence :</label>
+                                                        <select id="urgence" name="urgence" class="form-control">
                                                                 <option selected>Veuillez choisir une option</option>
                                                                 <option value="1">Ne pas faire </option>
                                                                 <option value="2">Est-ce vraiment à faire ? </option>
