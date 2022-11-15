0fe7765b55883b37917df8452a46c76b79b1e8db
Valentin <valentin.ctx@gmail.com>
2022-11-15 12:02:50 +0100

    e

--- profilmedecins.php
+++ profilmedecins.php
@@ -118,7 +118,7 @@ if (is_int($idmedecin))
         <tbody>
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                 <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Lyon
+                    <?php echo $profilmedecin['ville']; ?>
                 </th>
                 <td class="py-4 px-6">
                 <?php echo $profilmedecin['code_postal']; ?>
