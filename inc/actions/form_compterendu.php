a92b88e819ef03d202d6d4b5bb6d39c16f9a0c8c
Hrivaux <113358064+Hrivaux@users.noreply.github.com>
2022-11-21 07:21:03 +0100

    modif form ccompte rendu + affiche compteR

--- inc/actions/form_compterendu.php
+++ inc/actions/form_compterendu.php
@@ -8,13 +8,16 @@ $id_echantillon = $_POST["id_echantillon"];
 $date = $_POST["date"];
 $compterendu = $_POST["compterendu"];
 $avis = $_POST['avis'];
+$etat = $_POST['etat'];
 
 
-if (!empty($id_medecin) && !empty($id_echantillon) && !empty($date) && !empty($compterendu) && !empty($avis)) {
 
-   $reponse = $bdd->prepare("INSERT INTO comptesrendus(id_visiteur, id_medecin, date, id_echantillon, nouvelle_visite, compterendu, avis) VALUES (?,?,?,?,?, ?, ?)");
 
-   $reponse->execute(array($id_encours, $id_medecin, $date, $id_echantillon, '0', $compterendu, $avis));
+if (!empty($id_medecin) && !empty($id_echantillon) && !empty($date) && !empty($compterendu) && !empty($avis) && !empty($etat)) {
+
+   $reponse = $bdd->prepare("INSERT INTO comptesrendus(id_visiteur, id_medecin, date, id_echantillon, nouvelle_visite, compterendu, avis, etat) VALUES (?,?,?,?,?,?,?,?)");
+
+   $reponse->execute(array($id_encours, $id_medecin, $date, $id_echantillon, '0', $compterendu, $avis, $etat));
    Header('location: ../../accueil.php?action=successcomptesrendus');
 } 
 else
