371eb52f99a28b58d4e5ae87df60a029f0588bc1
Gabin <93249177+Gab1TN@users.noreply.github.com>
2022-11-21 16:28:55 +0100

    notif alertes

--- inc/actions/form_notification.php
+++ inc/actions/form_notification.php
@@ -14,39 +14,10 @@ if (!empty($objet) && !empty($msg) && !empty($urg)) {
 
     $reponse->execute(array($id_encours, $objet, $msg, $urg, $date));
 
-    //echo "<script type='script'> alert('Notification envoyée'); </script>";
-    header ('Location: ../../accueil.php?action=success');
+    header ('Location: ../../notifications.php?action=success');
 } 
 else 
 {
-   // echo "<script> alert('Merci de remplir tous les champs'); </script>";
-    Header('location: ../../form_notification.php?action=erreur');
+    Header('location: ../../notifications.php?action=erreur');
 }
-
-
-
 ?>
-	<!-- Modal box erreurs -->
-    <div class="modal fade" id="erreur" tabindex="-1" role="dialog" aria-hidden="true">
-	<div class="modal-dialog">
-		<div class="tbmodal">
-			<h3 style="color:white;">Une erreur est survenue, veuillez vérifier que tous les champs aient bien été remplis.</h3>
-		</div>
-	</div>
-</div>
-<?php
-	if(isset($_GET['action'])) 
-	{
-		$errlogin = htmlspecialchars($_GET['action']);
-		
-		switch($errlogin)
-		{
-			case 'erreur':
-?>
-<script>
-    $(document).ready(function()
-    {
-        $("#erreur").modal('show');
-    });
-</script>
-<?php break; } } ?>	
