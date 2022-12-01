22c846ea0048b95874db22297fd63491ae700d31
Gabin <93249177+Gab1TN@users.noreply.github.com>
2022-12-01 11:03:33 +0100

    accueil_correc

--- accueil.php
+++ accueil.php
@@ -85,7 +85,7 @@ include('templates/meta.php');
                                 <div class="col-xl-8 col-md-6">
                                     <div class="card Recent-Users">
                                         <div class="card-header">
-                                            <h5>Utilisateurs récents</h5>
+                                            <h5>Tous les utilisateurs</h5>
                                         </div>
                                         <div class="card-block px-0 py-3">
                                             <div class="table-responsive">
