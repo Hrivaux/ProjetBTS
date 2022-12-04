d3b16aa7f2063b0af83844eb66a42317f3a42909
Gabin <93249177+Gab1TN@users.noreply.github.com>
2022-12-04 22:17:33 +0100

    calculateur

--- inc/calculateur.php
+++ inc/calculateur.php
@@ -20,12 +20,12 @@ $nomgrade = $data['nom_grade'];
 if ($nbutilisateursgrade > 1) { $nomgrade = $nomgrade."s"; }
 
 // Nombre de comptes-rendus à saisir
-$nbCR1 = $bdd->query("SELECT count(*) as nb from comptesrendus WHERE etat = '0'");
+$nbCR1 = $bdd->query("SELECT count(*) as nb from comptesrendus WHERE etat = '0' AND id_visiteur = $id_encours");
 $data = $nbCR1->fetch();
 $nbcomptesrendusencours = $data['nb'];
 
 // Nombre de comptes-rendus terminés
-$nbCR = $bdd->query("SELECT count(*) as nb from comptesrendus WHERE etat = '1'");
+$nbCR = $bdd->query("SELECT count(*) as nb from comptesrendus WHERE etat = '1' AND id_visiteur = $id_encours");
 $data = $nbCR->fetch();
 $nbcomptesrendustermines = $data['nb'];
 ?>
