d30a57b21a72a3e39418c5c9a8c1b40593ccf834
Valentin <valentin.ctx@gmail.com>
2022-12-05 14:42:45 +0100

    e

--- inc/actions/form_medecins.php
+++ inc/actions/form_medecins.php
@@ -16,7 +16,7 @@ $personne_rattachee = $_POST['p_rattache'];
 
 if(isset($_POST['get_image'])) // $_POST['get_image'] - l'url de fichier source
 {
-$url=$_POST['get_image'];
+$url = $_POST['get_image'];
 $nomfichier = basename($_POST['get_image']);
 $data = file_get_contents($url);
 $new = '../../img/'.$nomfichier.''; //nom de fichier
